f4e5e65c5c82a402803e7b4cfa6b68aa1a9dc450
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-02-13 10:29:59 +0000

    Moved MLIR files onto feature branch.

---
+++ bitblaster_core/include/Bitblaster/BitblasterDialect.h
@@ -0,0 +1,16 @@
+//===- BitblasterDialect.h - Bitblaster dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef BITBLASTER_BITBLASTERDIALECT_H
+#define BITBLASTER_BITBLASTERDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Bitblaster/BitblasterOpsDialect.h.inc"
+
+#endif // BITBLASTER_BITBLASTERDIALECT_H
--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from __future__ import print_function
+
+import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print(
+        "Please update pip, you need pip 10 or greater,\n"
+        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        file=sys.stderr,
+    )
+    raise
+
 setup(
     name="bitblaster",
-    packages=["bitblaster"],
-    version="0.0.7",
+    version="0.0.6",
     description="A library for expressing and solving things in propositional logic.",
     author="Mark Kattenbelt",
     author_email="mark@mxklabs.co.uk",
     url="https://github.com/mxklabs/mxklabs-bitblaster",
     license="MIT",
+    packages=["bitblaster"],
+    package_dir={"": "src"},
+    cmake_install_dir="src/bitblaster",
     python_requires='>=3.6'
 )
